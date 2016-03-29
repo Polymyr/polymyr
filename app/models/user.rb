e6b13d30061cde323f7043c05ac3b6534e00585d
Michael Deng <michaeldeng18@gmail.com>
2016-03-29 02:16:21 -0700

    Add user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_many :products, dependent: :destroy
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "150x150>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
   
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true, length: { maximum: 50 } 
