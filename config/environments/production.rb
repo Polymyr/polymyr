46d39c760fac38b871d2da9ec6f2d39938ac8a61
Michael Deng <michaeldeng18@gmail.com>
2016-04-08 12:47:36 -0700

    Add S3 url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV['AWS_S3_REGION']
+      :url => 's3-' + ENV['AWS_S3_REGION']
     }
   }
 end
