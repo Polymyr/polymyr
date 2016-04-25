f460e01d1f65d04209f7d6be13af1723a4f7f070
Michael Deng <michaeldeng18@gmail.com>
2016-04-24 20:07:37 -0700

    Complete fundamental payment functionalities

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,33 +5,43 @@ class ChargesController < ApplicationController
 
 	def create
 
-		# if current_user.name.present? || !current_user.address.present? ||
-		# 	 !current_user.city.present? || !current_user.zip.present?
-		# 	current_user.update_attribute(:name, params[:name])
-		# 	current_user.update_attribute(:address, params[:address])
-		# 	current_user.update_attribute(:city, params[:city])
-		# 	current_user.update_attribute(:zip, params[:zip])
-		# end
+		@product = Product.find(params[:product_id])
 
-		# @product = Product.find(params[:product_id])
+		if !current_user.name.present? || !current_user.address.present? ||
+			 !current_user.city.present? || !current_user.zip.present?
+			current_user.update(:name => params[:name])
+		  current_user.update(:address => params[:address])
+		  current_user.update(:city => params[:city])
+		  current_user.update(:zip => params[:zip])
+		end
 
-	  customer = Stripe::Customer.create(
-	  	:email 	 => params[:stripeEmail],
-	    :source  => params[:stripeToken]
-	  )
+		if !current_user.customer_id.present?
+			customer = Stripe::Customer.create(
+		  	:email 	 => params[:stripeEmail],
+		    :source  => params[:stripeToken]
+		  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount 		 => 100,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+		  charge = Stripe::Charge.create(
+		    :customer    => customer.id,
+		    :amount 		 => @product.price * 100,
+		    :description => 'Rails Stripe customer',
+		    :currency    => 'usd'
+		  )
 
-	  # current_user.update_attribute(:customer_id, customer.id)
-	  # Review.create(user_id: current_user.id, product_id: @product.id)
+		  current_user.update(:customer_id => customer.id)
+		else
+			Stripe::Charge.create(
+				:customer 	 => current_user.customer_id,
+			  :amount   	 => @product.price * 100,
+			  :description => 'Rails Stripe customer',
+			  :currency 	 => "usd"
+			)
+		end
+
+	  Review.create(user_id: current_user.id, product_id: @product.id)
 
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
-	  # redirect_to @product
+	  redirect_to @product
 	end
 end
