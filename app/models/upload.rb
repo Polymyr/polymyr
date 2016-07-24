1ab90c3a5bc3df502b8145dcfb1a32dbb71e427f
Michael Deng <michaeldeng18@gmail.com>
2016-07-23 17:36:06 -0700

    Polish product dashboard page

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,10 +2,10 @@ class Upload < ActiveRecord::Base
 
 	belongs_to :product
 
-	has_attached_file :image, :styles => { :large => "640x480#", medium: "320x240#", :thumb => "50x50#" }
+	has_attached_file :image, :styles => { :large => "640x480#", medium: "320x240#", :thumb => "60x45#" }
 	
-	validates_attachment :image, 
-				presence: true, 
+	validates_attachment :image,
+				presence: true,
 				content_type: { content_type: /\Aimage\/.*\Z/ },
 				size: { :less_than => 1.megabyte }
 end
