507e74c35d97bc5943a2f475378fbea2b3d7e590
Michael Deng <michaeldeng18@gmail.com>
2016-08-24 15:50:09 -0700

    Fix adaptive labels not showing up for inputs with existing content

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+	$(".form-wrapper .form-inputs input").each(function() {
+		if ($(this).val()) {
+			$(this).prev().css({"margin-top": "3px",
+	  										"opacity": "1",
+	  										"visibility": "visible"});
+		}
+	});
+
 	// Adaptive placeholders
   $(".form-wrapper .form-inputs input").focusin(function () {
 	  $(this).prev().css({"margin-top": "3px",
