d10a183d18583864fb306a90a36dfb4ebb497e11
Michael Deng <michaeldeng18@gmail.com>
2016-08-26 12:07:47 -0700

    Add $ to price and rebate fields

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -5,6 +5,14 @@ $(document).ready(function() {
 			$(this).prev().css({"margin-top": "3px",
 	  										"opacity": "1",
 	  										"visibility": "visible"});
+			if ($(this).is("#product_price")) {
+				$(this).css("padding-left", "0.8em");
+				$("#price-dollar-sign").css("display", "block");
+			}
+			if ($(this).is("#product_rebate")) {
+				$(this).css("padding-left", "0.8em");
+				$("#rebate-dollar-sign").css("display", "block");
+			}
 		}
 	});
 
@@ -13,6 +21,14 @@ $(document).ready(function() {
 	  $(this).prev().css({"margin-top": "3px",
 	  										"opacity": "1",
 	  										"visibility": "visible"});
+	  if ($(this).is("#product_price")) {
+			$(this).css("padding-left", "0.8em");
+			$("#price-dollar-sign").css("display", "block");
+		}
+		if ($(this).is("#product_rebate")) {
+			$(this).css("padding-left", "0.8em");
+			$("#rebate-dollar-sign").css("display", "block");
+		}
 	});
 
 	$(".form-wrapper .form-inputs input").focusout(function () {
@@ -20,6 +36,14 @@ $(document).ready(function() {
 	  	$(this).prev().css({"margin-top": "8px",
 		  										"opacity": "0",
 		  										"visibility": "collapse"});
+	  	if ($(this).is("#product_price")) {
+				$(this).css("padding-left", "0");
+				$("#price-dollar-sign").css("display", "none");
+			}
+			if ($(this).is("#product_rebate")) {
+				$(this).css("padding-left", "0");
+				$("#rebate-dollar-sign").css("display", "none");
+			}
 	  }
 	});
 
